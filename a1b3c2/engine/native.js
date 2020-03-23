4023c182e3a38968877e317c4a04efa4e3acf8b0
AMazelin <armazelin@gmail.com>
2020-03-22 19:30:30 -0500

    Push beta churchlisten to personal site.

---
+++ a1b3c2/engine/native.js
@@ -0,0 +1,39 @@
+// native.js -- Provides native functions for churchlisten
+// This module is responsible for url lookups and playing the music
+var element;
+
+function requestJSON(url, callback) {
+    window._callback = callback;
+    url = encodeURI(url);
+
+    // Borrowed from http://forums.mozillazine.org/viewtopic.php?t=48738
+    if (element != null) {
+        document.getElementById('scriptid').remove();
+    };
+    element = document.createElement("script");
+    element.src = "https://json2jsonp.com/?url=" + url + "&callback=window._callback";
+    element.type = "text/javascript";
+    element.id = "scriptid";
+    document.getElementsByTagName("head")[0].appendChild(element);
+}
+
+// churchPlayback -- Adds playback functionallity to churchListen
+function churchPlayback(audioelement) {
+    this.audioelement = audioelement;
+    churchListen.call(this);  
+};
+
+churchPlayback.prototype = Object.create(churchListen.prototype);
+churchPlayback.prototype.constructor = churchListen;
+
+churchPlayback.prototype.loadCongregation = function(state,congregation) {
+    churchListen.prototype.loadCongregation.call(this,state,congregation);
+    this.audioelement.src = this.getStreamingUrl();
+    this.audioelement.load();
+    this.audioelement.play();
+}
+
+churchPlayback.prototype.unloadCongregation = function() {
+    churchListen.prototype.unloadCongregation.call(this);
+    this.audioelement.pause();
+}
---
+++ a1b3c2/main.js
@@ -0,0 +1,61 @@
+//const api = new churchListen();
+function loadState(state) {
+    window.state = state;
+    document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: state}});
+};
+
+function loadCongregation(congregation) {
+    window.congregation = congregation;
+    document.querySelector('#myNavigator').pushPage('page3.html', {data: {title: congregation}});
+};
+
+function handle_status (result) {
+    document.getElementById("status").innerText = result["message"] + ".";
+};
+
+document.addEventListener('init', function(event) {
+    var page = event.target;
+
+    if (window.api === undefined) {
+        window.api = new churchPlayback(document.getElementById("testaudio"));
+    }
+
+    if (page.id === 'page1') {
+        string = "";
+        window.api.listStates().forEach(function (state, stateindex) {
+            string = string + '<ons-list-item onclick="loadState(' + "'" + state + "')" + ';">' + state + '</ons-list-item>';
+        });    
+        page.querySelector("#states").innerHTML = string;
+    } else if (page.id === 'page2') {
+        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+        string = "";
+        window.api.listCongregations(window.state).forEach(function (congregation, congregationindex) {
+            string = string + '<ons-list-item onclick="loadCongregation(' + "'" + congregation + "')" + ';">' + congregation + '</ons-list-item>';
+        });
+        page.querySelector("#places").innerHTML = string
+    } else if (page.id === 'page3') {
+        window.api.loadCongregation(window.state,window.congregation);
+        window.status_indicator = window.setInterval(function () {
+            window.api.getStatus(handle_status);
+            return true;
+        },2000);
+        page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+        page.querySelector('#backcongregations').innerHTML = window.state;
+        page.querySelector('ons-back-button').onClick = function(event) {
+            console.log("Cancelling music.");
+            window.api.unloadCongregation();
+            window.clearInterval(window.status_indicator);
+            document.querySelector('ons-navigator').popPage()
+        };
+        window.api.getStatus(handle_status);
+
+    };
+});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./a1b3c2/sw.js', { scope: 'a1b3c2/' }).then(function(reg) {
+        console.log('Successfully registered service worker.', reg);
+    }).catch(function(err) {
+        console.warn('Error whilst registering service worker', err);
+    });
+}
